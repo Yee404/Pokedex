250b0006480a88088b5a63f057113a1d9190b511
Yee404 <yee231193@uvg.edu.gt>
2023-09-29 08:54:21 -0600

    :P

---
+++ Pikachu.java
@@ -0,0 +1,20 @@
+public class Pikachu extends Pokemon implements Electrico {
+
+    private String nombreElectrico;
+
+    public Pikachu(String nombre, String tipo, int numero, int generacion, String impactTrueno, String ondaTrueno){
+
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.numero = numero;
+        this.generacion = generacion;
+        this.impactTrueno = impactTrueno;
+        this.ondaTrueno = ondaTrueno;
+
+    }
+
+    public String Ataque(){
+        
+    }
+
+}
--- Pokedex/Electrico.java
+++
@@ -1,6 +0,0 @@
-public interface Electrico {
-
-    public String impactTrueno();
-    public String ondaTrueno();
-    
-}
